d2e94cb2c92240e4858e3bc9e0fc13d89c62f04d
Amandeep Singh <amandeepspdhr@gmail.com>
2020-12-09 11:28:37 +0530

    Day 8 solutions (no brute force)

---
+++ src/aoc/day8.clj
@@ -0,0 +1,78 @@
+(ns aoc.day8
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(defn parse-line [line]
+  (let [[opcode val] (str/split line #" ")]
+    (vector (keyword opcode) (Integer/parseInt val))))
+
+(defn parse-input [file-name]
+  (->> (slurp (io/resource file-name))
+       (str/split-lines)
+       (mapv parse-line)))
+
+(defn run-machine [ops]
+  (loop [pc 0 acc 0 visited #{}]
+    (cond
+      (visited pc) (vector :loop acc)
+      (= pc (count ops)) (vector :term acc)
+      :else (let [[op val] (get ops pc)]
+              (case op
+                :nop (recur (inc pc) acc (conj visited pc))
+                :acc (recur (inc pc) (+ acc val) (conj visited pc))
+                :jmp (recur (+ pc val) acc (conj visited pc)))))))
+
+(defn part-1 [file-name]
+  (let [ops (parse-input file-name)]
+    (run-machine ops)))
+
+(defn find-error [ops start state]
+  (loop [pc start visited #{}]
+    (cond
+      (visited pc) (-> state
+                       (update :unvisited #(remove visited %))
+                       (update :looping-ops into visited))
+      (= pc (count ops)) (-> state
+                             (update :unvisited #(remove visited %))
+                             (update :halting-ops into visited))
+      :else (let [[op val] (get ops pc)]
+              (case op
+                :nop (recur (inc pc) (conj visited pc))
+                :acc (recur (inc pc) (conj visited pc))
+                :jmp (recur (+ pc val) (conj visited pc)))))))
+
+(defn classify-ops [ops]
+  (let [init-state {:halting-ops #{}
+                    :looping-ops #{}
+                    :unvisited   (range (count ops))}]
+    (loop [state init-state]
+      (if (empty? (:unvisited state))
+        state
+        (recur (find-error
+                 ops
+                 (first (:unvisited state))
+                 (update state :unvisited next)))))))
+
+(defn correct-ops [ops]
+  (let [{:keys [halting-ops]} (classify-ops ops)]
+    (loop [index 0]
+      (let [[opcode val] (nth ops index)]
+        (cond
+          (= index (count ops)) :correct-ops
+          (and (= :jmp opcode) (halting-ops (inc index))) (assoc ops index [:nop val])
+          (and (= :nop opcode) (halting-ops (+ index val))) (assoc ops index [:jmp val])
+          :else (recur (inc index)))))))
+
+(defn part-2 [file-name]
+  (let [ops (parse-input file-name)
+        correct-ops (correct-ops ops)]
+    (run-machine correct-ops)))
+
+(prn (part-1 "day8.txt"))
+;; => 2003
+
+(prn (part-2 "day8.txt"))
+;; => 1984
+
+
+
---
+++ src/aoc/day9.clj
@@ -0,0 +1,1 @@
+(ns aoc.day9)
